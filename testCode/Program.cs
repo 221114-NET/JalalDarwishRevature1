28509e60b7d789ce83799514ab35deaa87b34244
unknown <jdarwish22@gmail.com>
2022-11-21 15:12:15 -0600

    Setup of test project

---
+++ testCode/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace testCode;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string emptyTest = "";
+        TestStuff.EmptyTest(emptyTest);
+    }
+}
+
+class TestStuff
+{
+    public static void EmptyTest(string s)
+    {
+        Console.WriteLine(s);
+    }
+}
